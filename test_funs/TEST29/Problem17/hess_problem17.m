53282a5b907723433f5c69f29f5aeb344d3b20f1
b-gebken <115168210+b-gebken@users.noreply.github.com>
2023-07-19 15:30:32 +0200

    Update hess_problem17.m

--- test_funs/TEST29/Problem17/hess_problem17.m
+++ test_funs/TEST29/Problem17/hess_problem17.m
@@ -7,7 +7,7 @@ function hess = hess_problem17(x)
     
     i_max = I(n+1);
     j = floor((i_max-1)/5);
-    hess(i_max,i_max) = I(i_max)*(-cos(x(i_max))+sin(x(i_max)));
+    hess(i_max,i_max) = I(i_max)*(-(j+1)*cos(x(i_max))+sin(x(i_max)));
     for k = 5*j+1:5*j+5
         hess(k,k) = hess(k,k) + I(i_max)*(-(-cos(x(k))));
     end
